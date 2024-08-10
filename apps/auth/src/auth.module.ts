5d2830de71812f85e86e52e38857253798ba92ac
Umut Yenidil <umut.yenidil9@gmail.com>
2024-08-10 12:04:41 +0300

    postgresql yapilandirildi

--- apps/auth/src/auth.module.ts
+++ apps/auth/src/auth.module.ts
@@ -1,13 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: "./.env",
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        url: configService.get("POSTGRES_URI"),
+      }),
+      inject: [ConfigService],
     }),
   ],
   controllers: [AuthController],
